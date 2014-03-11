73e9a4e8ad1139e1e7e3efc11fcc3d060e1305c3
Pete Woods <pete.woods@canonical.com>
2014-03-11 15:17:20 +0000

    Don't know what these are for

--- utils/helper-exec-tool.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -e "~/.cache/infographic-service/visualizers/${APP_ID}" ] ; then
-	initctl set-env "APP_EXEC=echo Visualizer ${APP_ID} started"
-else
-	echo "No visualizer for ${APP_ID}"
-	exit -1
-fi
